3efade4bd0adf39759d4e82c6e919b373475996d
Francisco Garcia <frgarcia@ucsd.edu>
2022-02-03 16:51:55 -0800

    Libs folder + initial failed test and code

---
+++ practiceOne.java
@@ -0,0 +1,6 @@
+public class practiceOne {
+    // Yet to edit in VSCode
+    public static int subtract (int x, int y) {
+        return x - y;
+    }
+} 
---
+++ practiceOneTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class practiceOneTest {
+    // Add tests here
+    
+
+    @Test
+    public void subtraction() {
+        assertEquals(2, practiceOne.subtract(1,2) );
+    }
+
+}
